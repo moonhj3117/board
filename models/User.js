6d5819ab2eb7e98fed6a6408b2c434427451c6bd
moonhanjoo <moonhj3117@hanmail.net>
2017-01-25 15:41:28 +0900

    bcrypt commit

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 // models/User.js
 
 var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
 
 // schema // 1
 var userSchema = mongoose.Schema({
@@ -48,13 +49,24 @@ userSchema.path("password").validate(function(v) {
   if(!user.currentPassword){
    user.invalidate("currentPassword", "Current Password is required!");
   }
-  if(user.currentPassword && user.currentPassword != user.originalPassword){
+  if(user.currentPassword && !bcrypt.compareSync(user.currentPassword , user.originalPassword)){
    user.invalidate("currentPassword", "Current Password is invalid!");
   }
   if(user.newPassword !== user.passwordConfirmation) {
    user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
   }
  }
+});
+
+//hash password
+userSchema.pre("save", function(next){
+  var user = this;
+  if(!user.isModified("password")){
+    return next();
+  } else {
+    user.password = bcrypt.hashSync(user.password);
+    return next();
+  }
 });
 
 // model & export
